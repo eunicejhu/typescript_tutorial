05995d9cada45421fd8e26c82a6299eca07929c7
Isabella Hu <eunicejhu@gmail.com>
2020-01-04 20:44:22 +0800

    rewrite webpack config using typescript

---
+++ build/webpack.config.ts
@@ -0,0 +1,31 @@
+import htmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+
+const config: webpack.Configuration = {
+  devServer: {
+    compress: false,
+    contentBase: "./dist",
+    host: "localhost",
+    port: 9000
+  },
+  devtool: process.env.NODE_ENV === "production" ? false : "inline-source-map",
+  entry: "./src/main.ts",
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: "ts-loader"
+      }
+    ]
+  },
+  output: { filename: "build.js" },
+  resolve: { extensions: [".tsx", ".ts", ".js"] },
+
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "index.html"
+    })
+  ]
+};
+export default config;
